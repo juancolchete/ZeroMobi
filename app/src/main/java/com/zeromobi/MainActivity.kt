720da263389b1c125a0f85c37a5bafd12455e16a
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2022-05-24 19:20:38 -0300

    Add Latest layout changes

--- app/src/main/java/com/zeromobi/MainActivity.kt
+++ app/src/main/java/com/zeromobi/MainActivity.kt
@@ -2,10 +2,17 @@ package com.zeromobi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.pickup)
+
+        var btnLogin = findViewById<Button>(R.id.btn_login)
+        btnLogin.setOnClickListener {
+            setContentView(R.layout.pickup)
+        }
     }
 }
---
+++ app/src/main/java/com/zeromobi/PickUp.kt
@@ -0,0 +1,4 @@
+package com.zeromobi
+
+class PickUp {
+}
